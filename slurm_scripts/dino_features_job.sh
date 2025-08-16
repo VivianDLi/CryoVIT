de0692cee34fb5c1fc44f4e818cac80408dd8ec8
Vivian Li <vdl21@cantab.ac.uk>
2025-08-15 17:18:23 -0700

    adjusted dino_features.sh paths to fit current directory and added more comprehensive test experiments

--- slurm_scripts/dino_features_job.sh
+++ slurm_scripts/dino_features_job.sh
@@ -8,6 +8,8 @@ tar -xf ~/projects/libs/cryovit_env.tar -C $env_dir
 
 $env_dir/cryovit_env/bin/python -m \
     cryovit.dino_features \
-    sample=$sample
+    sample=$sample \
+    paths.tomo_name=processed_with_annot \
+    paths.feature_name=tomograms
 
 rm -rf $env_dir
