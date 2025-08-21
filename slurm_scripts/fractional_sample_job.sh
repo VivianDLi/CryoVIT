24411bd21b4ea92dd5d318a038940312ae7b685b
Vivian Li <vdl21@cantab.ac.uk>
2025-08-20 18:56:24 -0700

    implemented experiment + scripts for fractional dataset experiment

--- slurm_scripts/fractional_sample_job.sh
+++ slurm_scripts/fractional_sample_job.sh
@@ -2,16 +2,20 @@
 
 sample=$1
 split_id=$2
-sample_group=$3
-model=$4
-label_key=$5
+model=$3
+label_key=$4
+
+# Setup W&B API key
+if [ -z "$WANDB_API_KEY" ]; then
+    export WANDB_API_KEY=$5
+fi
 
 env_dir=/sdf/home/v/vdl21/projects/libs/
 
 $env_dir/cryovit_env/bin/python -m \
     cryovit.train_model \
     model=$model \
-    name="fractional_${sample_group}_${model}_${label_key}" \
+    name="fractional_${sample}_${model}_${label_key}" \
     label_key=$label_key \
     datamodule=fractional_loo \
     datamodule.sample=$sample \
